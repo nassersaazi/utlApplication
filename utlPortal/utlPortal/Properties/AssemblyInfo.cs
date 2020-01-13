569f97ac13206de7a491f14dfde8d0bd23c59892
nassersaazi <nassersnoop@gmail.com>
2020-01-13 09:16:50 +0300

    Cleaned up the project by deleting some code smells

--- utlPortal/utlPortal/Properties/AssemblyInfo.cs
+++ utlPortal/utlPortal/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("utl2020.Tests")]
+[assembly: AssemblyTitle("utlPortal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("utl2020.Tests")]
+[assembly: AssemblyProduct("utlPortal")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7b1bbf17-165d-411c-a422-54e7e4b67ad0")]
+[assembly: Guid("b967ab8a-b6cb-4c52-a6a3-7aa817ef9762")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
